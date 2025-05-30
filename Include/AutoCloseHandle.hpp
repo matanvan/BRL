40ba4f4cdb70035f3d6e15d1dd1902008e172129
matanvan <matanvan@gmail.com>
2025-05-30 23:06:56 +0300

    meow

--- Include/AutoCloseHandle.hpp
+++ Include/AutoCloseHandle.hpp
@@ -4,7 +4,7 @@
 #include <memory>
 #include <functional>
 
-class AutoCloseHandle: private std::unique_ptr<std::remove_pointer_t<HANDLE>, BOOL(*)(HANDLE)>
+class AutoCloseHandle: public std::unique_ptr<std::remove_pointer_t<HANDLE>, BOOL(*)(HANDLE)>
 {
 public:
 	AutoCloseHandle(HANDLE handle) :
@@ -14,6 +14,4 @@ public:
 	~AutoCloseHandle() = default;
 	AutoCloseHandle(const AutoCloseHandle&) = delete;
 	AutoCloseHandle& operator=(const AutoCloseHandle&) = delete;
-
-	inline HANDLE get() const { return std::unique_ptr<std::remove_pointer_t<HANDLE>, BOOL(*)(HANDLE)>::get(); }
 };
